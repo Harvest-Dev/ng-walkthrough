d6d5ed0e0f17a9ce02a86c03d022c5b55353c5da
zefling <zefling@ikilote.net>
2017-10-15 15:39:07 +0200

    Make WalkthroughFlow usable

--- src/walkthrough-flow.component.ts
+++ src/walkthrough-flow.component.ts
@@ -1,18 +1,37 @@
 import {
-    Component
+    Component,
+    ContentChildren,
+    QueryList,
+    AfterViewInit
 } from '@angular/core';
+import { WalkthroughComponent } from './walkthrough.component';
 
 @Component({
     selector: 'ng-walkthrough-flow',
     template: ''
 })
-export class WalkthroughFlowComponent {
+export class WalkthroughFlowComponent implements AfterViewInit {
+
+    @ContentChildren(WalkthroughComponent) walkthroughComponents: QueryList<WalkthroughComponent>;
 
     constructor(
     ) { }
 
+    ngAfterViewInit() {
+        setTimeout(() => {
+            let prevComp: WalkthroughComponent = null;
+            this.walkthroughComponents.forEach((walkthrough: WalkthroughComponent) => {
+                if (prevComp) {
+                    walkthrough.previousStep = prevComp;
+                    prevComp.nextStep = walkthrough;
+                }
+                prevComp = walkthrough;
+            });
+            prevComp.finishStep = true;
+        }, 0);
+    }
 
     start() {
-
+        this.walkthroughComponents.first.open();
     }
 }
