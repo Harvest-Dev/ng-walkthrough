a4c5f0bdb8a1aa8ffae4245df5770319b152f536
cvs <celian-veyssiere@harvest.fr>
2017-12-27 16:06:05 +0100

    Update Demo

--- demo/scripts.js
+++ demo/scripts.js
@@ -1,11 +1,11 @@
 webpackJsonp([2],{
 
-/***/ 403:
+/***/ 254:
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(34);
+module.exports = __webpack_require__(57);
 
 
 /***/ })
 
-},[403]);
+},[254]);
