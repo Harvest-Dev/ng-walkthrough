e7c746990a19a924db0130c3aca2229ba018a812
Célian Veyssière <celian-veyssiere@harvest.fr>
2018-02-20 16:42:12 +0100

    Add walkthrough-flow in index.ts #6

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export * from './walkthrough.component';
+export * from './walkthrough-flow.component';
 export * from './walkthrough-container.component';
 export * from './walkthrough-text';
 export * from './walkthrough.module';
