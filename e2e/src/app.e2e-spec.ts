98aee67bf64486462f2b29401256b370f936bc53
Baptiste MENADIER <baptiste-menadier@harvest.fr>
2018-06-22 11:34:40 +0200

    fix e2e test

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -7,8 +7,4 @@ describe('workspace-project App', () => {
     page = new AppPage();
   });
 
-  it('should display welcome message', () => {
-    page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!');
-  });
 });
