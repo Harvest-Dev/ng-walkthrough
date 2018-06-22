bce4923f7e58616fb66dcd0339c30a4513bd6afe
Baptiste MENADIER <baptiste-menadier@harvest.fr>
2018-06-22 10:38:42 +0200

    fix css rendering

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,7 +9,7 @@ import {
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   frenchText: WalkthroughText = {
