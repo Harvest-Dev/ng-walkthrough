f67b2cf85d50c9e2a713792b80059b52ee0b3873
Baptiste MENADIER <baptiste-menadier@harvest.fr>
2018-06-22 14:18:50 +0200

    update demo

--- dist/ng-walkthrough/main.js
+++ dist/ng-walkthrough/main.js
@@ -2363,7 +2363,7 @@ Object(_angular_platform_browser_dynamic__WEBPACK_IMPORTED_MODULE_1__["platformB
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! c:\Users\BMenadier\dev\projects\ng-walkthrough\src\main.ts */"./src/main.ts");
+module.exports = __webpack_require__(/*! C:\Users\BMenadier\dev\projects\ng-walkthrough\src\main.ts */"./src/main.ts");
 
 
 /***/ })
--- dist/ng-walkthrough/styles.js
+++ dist/ng-walkthrough/styles.js
@@ -539,7 +539,7 @@ if(false) {}
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! c:\Users\BMenadier\dev\projects\ng-walkthrough\src\styles.css */"./src/styles.css");
+module.exports = __webpack_require__(/*! C:\Users\BMenadier\dev\projects\ng-walkthrough\src\styles.css */"./src/styles.css");
 
 
 /***/ })
