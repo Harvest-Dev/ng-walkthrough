fbab2d3b915e8cfb308c77a074a200c4ad3b5131
Baptiste MENADIER <baptiste-menadier@harvest.fr>
2018-06-22 09:14:25 +0200

    insert demo into Angular application

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AngularWalkthroughModule } from 'angular-walkthrough';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    AngularWalkthroughModule,
     BrowserModule
   ],
   providers: [],
